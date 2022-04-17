0610ba09b27c0e2da92938333cef47ca2bb8b679
karimismailgenesys <99881083+karimismailgenesys@users.noreply.github.com>
2022-04-17 14:22:46 +0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -8,13 +8,12 @@
   g[e].t = 1 * new Date();
   g[e].c = es;
   ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+  document.getElementById("start").style.backgroundColor="#000000";
+  document.getElementById("start").style.color="#000000";
 })(window, 'Genesys', 'https://apps.mypurecloud.ie/genesys-bootstrap/genesys.min.js', {
   environment: 'euw1',
   deploymentId: '8535b668-213d-4ee6-9786-023aadb614b7'
 });
-
-document.getElementById("start").style.backgroundColor="#000000";
-document.getElementById("start").style.color="#000000";
 
 
 function myFunction() {
