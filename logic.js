a8ba12d1a1959998ffaa40d8dbb6b36c1919017a
karimismailgenesys <99881083+karimismailgenesys@users.noreply.github.com>
2022-04-17 14:16:55 +0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -4,8 +4,6 @@
   g['_genesysJs'] = e;
   g[e] = g[e] || function () {
     (g[e].q = g[e].q || []).push(arguments)
-    document.getElementById("start").style.backgroundColor="#000000";
-    document.getElementById("start").style.color="#000000";
   };
   g[e].t = 1 * new Date();
   g[e].c = es;
@@ -15,6 +13,8 @@
   deploymentId: '8535b668-213d-4ee6-9786-023aadb614b7'
 });
 
+document.getElementById("start").style.backgroundColor="#000000";
+document.getElementById("start").style.color="#000000";
 
 
 function myFunction() {
