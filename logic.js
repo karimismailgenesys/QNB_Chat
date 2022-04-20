fefb392c1ee9118e68ba8112a9b28d34015b7585
karimismailgenesys <99881083+karimismailgenesys@users.noreply.github.com>
2022-04-20 18:02:04 +0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -19,10 +19,7 @@
   deploymentId: '8535b668-213d-4ee6-9786-023aadb614b7'
 });
 
-function myFunction(){
-  Genesys("command", "Messenger.open");
-}
-
+console.log("SUBSCRIBING..");
 Genesys("subscribe", "Messenger.ready", function(){
     console.log("INSIDE THE READY BLOCK");
     Genesys("command", "Messenger.open")});
