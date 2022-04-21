80c50721201d6349d23bd8b47a33c578616b983f
karimismailgenesys <99881083+karimismailgenesys@users.noreply.github.com>
2022-04-21 12:51:55 +0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,19 +1,17 @@
 
 
-(function (g, e, n, es, ys) {
-  alert("1 INSIDE FUNCTION");
-  g['_genesysJs'] = e;
-  g[e] = g[e] || function () {
-    alert("2 BEFORE PUSH ARGUMENTS");
-    (g[e].q = g[e].q || []).push(arguments)
-  };
-  g[e].t = 1 * new Date();
-  g[e].c = es;
-  ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
-})(window, 'Genesys', 'https://apps.mypurecloud.ie/genesys-bootstrap/genesys.min.js', {
-  environment: 'euw1',
-  deploymentId: '8535b668-213d-4ee6-9786-023aadb614b7'
-});
+  (function (g, e, n, es, ys) {
+    g['_genesysJs'] = e;
+    g[e] = g[e] || function () {
+      (g[e].q = g[e].q || []).push(arguments)
+    };
+    g[e].t = 1 * new Date();
+    g[e].c = es;
+    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+  })(window, 'Genesys', 'https://apps.mypurecloud.ie/genesys-bootstrap/genesys.min.js', {
+    environment: 'euw1',
+    deploymentId: '8535b668-213d-4ee6-9786-023aadb614b7'
+  });
 
 
 
