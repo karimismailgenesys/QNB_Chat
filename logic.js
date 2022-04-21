a3b405e6939350d31c1678c65e651e8a6948dcc7
karimismailgenesys <99881083+karimismailgenesys@users.noreply.github.com>
2022-04-21 12:47:10 +0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -10,21 +10,14 @@
   g[e].t = 1 * new Date();
   g[e].c = es;
   ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
-  console.log("AFTER CREATE ELEMENT");
-  console.log("END OF FUCNTION");
 })(window, 'Genesys', 'https://apps.mypurecloud.ie/genesys-bootstrap/genesys.min.js', {
   environment: 'euw1',
   deploymentId: '8535b668-213d-4ee6-9786-023aadb614b7'
 });
 
-alert("3 SUBSCRIBING..");
 
-//document.onload = Genesys("command", "Messenger.open");
 
-//Genesys("subscribe", "Messenger.ready");
-
-/*function myFunction(){
-    alert("4 INSIDE THE READY BLOCK");
+Genesys("subscribe", "Messenger.ready", function(o) {
     Genesys("command", "Messenger.open");
-}*/
+});
 
